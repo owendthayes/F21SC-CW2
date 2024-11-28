2420d0cb0e06f77d9c25f8151b0a9535426f5f9d
jd2030 <jd2030@hw.ac.uk>
2024-11-28 20:20:43 +0000

    progress

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import json
+import tkinter
+import pandas as pd
+import re
 
 FILE_NAME = 'test.json'
 
+#reading in data
 with open(FILE_NAME, 'r') as file:
     data = json.load(file)
 
-for i in data:
-    print("\n\n", i)
+def search_country(document):
+    # "subject_doc_id"
+    countries = [obj.get("visitor_country") for obj in data if (obj.get("subject_doc_id") == document)]
+    search_continent(countries)
+    print(pd.Series(countries).value_counts()) #DEBUGGING
+    return pd.Series(countries).value_counts()
+
+def search_continent(countries):
+    return
+
+def views_by_browser():
+    browsers = []
+
+    #populating all browsers into list
+    for obj in data:
+        browserName = obj.get("visitor_useragent").split('/')
+        browsers.append(browserName[0])
+
+    #totalling number of users on each browser
+    freq = pd.Series(browsers).value_counts()
+
+    #returning most popular browser
+    print(freq) #DEBUGGING
+    return freq
+
+
+search_country("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0")
+views_by_browser()
