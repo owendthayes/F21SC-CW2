b186c7067d8ab6e763c9d958ad7a708a87c9ec01
jd2030 <jd2030@hw.ac.uk>
2024-11-30 01:14:39 +0000

    search continent works and file will now be edited to work properly automatically

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import json
 import tkinter
 import pandas as pd
 import re
-#from pycountry_convert import country_alpha2_to_continent_code, convert_continent_code_to_continent_name
+from pycountry_convert import country_alpha2_to_continent_code, convert_continent_code_to_continent_name
 
 
 FILE_NAME = 'test.json'
-
 #reading in data
 with open(FILE_NAME, 'r') as file:
+    lines = [line.strip() for line in file if line.strip()]
+fixed_json = "[\n" + ",\n".join(lines) + "\n]"
+with open('fixed_file.json', 'w') as file:
+    file.write(fixed_json)
+with open('fixed_file.json', 'r') as file:
     data = json.load(file)
 
-def search_country(document):
-    # "subject_doc_id"
+def search_country(document):# "subject_doc_id"
     countries = [obj.get("visitor_country") for obj in data if (obj.get("subject_doc_id") == document)]
     search_continent(countries)
     print(pd.Series(countries).value_counts()) #DEBUGGING
     return pd.Series(countries).value_counts()
 
 def search_continent(countries):
-    return
+    continents = [convert_continent_code_to_continent_name(country_alpha2_to_continent_code(country)) for country in countries]
+    print(pd.Series(continents).value_counts()) #DEBUGGING
+    return pd.Series(continents).value_counts()
 
 def views_by_browser():
     browsers = []
@@ -35,7 +40,6 @@ def views_by_browser():
     #returning most popular browser
     print(freq) #DEBUGGING
     return freq
-
 
 search_country("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0")
 views_by_browser()
