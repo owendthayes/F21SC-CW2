56a4a7118b6d1c20277560dde3029028ea0de061
Owen Hayes <owendthayes@gmail.com>
2024-12-06 18:47:02 +0000

    tried to get user input working, sorta there.

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import json
 from tkinter import * 
 import pandas as pd
 import re
+import pycountry
 from pycountry_convert import country_alpha2_to_continent_code, convert_continent_code_to_continent_name
 import matplotlib
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-FILE_NAME = 'test.json'
+FILE_NAME = "\\" + txtLoadFile.get("1.0", END)
+print(FILE_NAME)
 #reading in data
 with open(FILE_NAME, 'r') as file:
     lines = [line.strip() for line in file if line.strip()]
@@ -31,6 +33,9 @@ def gui():
     bottom_frame = Frame(root) #creates frame area for the bottom row (used as button area)
     bottom_frame.pack(side="bottom", fill="x") #fills from the bottom left
 
+    top_frame = Frame(root)
+    top_frame.pack(side =  "top", fill = "x")
+
     top_frame = Frame(root) #creates frame area for the top row (used as text area)
     top_frame.pack(side="top", fill="x") #fills from the top left
 
@@ -43,6 +48,25 @@ def gui():
     def clicked_search_doc_continent():
         create_bar_chart(search_continent("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0"), "140228202800-6ef39a241f35301a9a42cd0ed21e5fb0", "Visitor frequency from each continent for document","Country", "Frequency")
 
+
+##ngl idk if this is needed, we might just need to do it in command line? im not rly sure
+    # def load_input_file():
+    #     try:
+    #         FILE_NAME = "\\" + txtLoadFile.get("1.0", END)
+    #         print(FILE_NAME)
+    #         #reading in data
+    #         with open(FILE_NAME, 'r') as file:
+    #             lines = [line.strip() for line in file if line.strip()]
+    #         fixed_json = "[\n" + ",\n".join(lines) + "\n]"
+    #         with open('fixed_file.json', 'w') as file:
+    #             file.write(fixed_json)
+    #         with open('fixed_file.json', 'r') as file:
+    #             data = json.load(file)
+    #     except Exception as e:
+    #         print(repr(e))
+
+
+
     btn_view_document_continent = Button(bottom_frame, text = "Hit me", font = ("Arial", 14) ,fg= "red", command=clicked_search_doc_continent)
     btn_view_document_continent.pack(side = "left")
 
@@ -54,6 +78,14 @@ def gui():
 
     btn5 = Button(bottom_frame, text = "Hit me", font = ("Arial", 14) ,fg= "red", command=clicked_search_doc_country)
     btn5.pack(side = "left")
+
+
+    ##CHOOSING INPUT FILE
+    txtLoadFile = Text(top_frame, font = ("Arial", 14), fg = "blue", height = 1.4, width = 30)
+    txtLoadFile.pack(side = "left", anchor = "nw")
+
+    btnLoadFile = Button(top_frame, text = "select file", font = ("Arial", 14) ,fg= "blue", command=load_input_file)
+    btnLoadFile.pack(side = "left", anchor = "nw")
 
     root.mainloop()
 
@@ -88,8 +120,24 @@ def search_continent(document):
     print(pd.Series(continents).value_counts()) #DEBUGGING
     return pd.Series(continents).value_counts() #returns a tallied up version of the list showing the continents and frequency of visitors
 
-##REQ 3
-def views_by_browser():
+##REQ 3 A e.g. entire useragent string
+def views_by_browser_verbose():
+    browsers = []
+
+    #populating all browsers into list
+    for obj in data:
+        browsers.append(obj.get("visitor_useragent"))
+
+    #totalling number of users on each browser
+    freq = pd.Series(browsers).value_counts()
+
+    #returning most popular browser
+    print(browsers)
+    print(freq) #DEBUGGING
+    return freq
+
+##REQ 3 B e.g. 'Mozilla'
+def views_by_browser_short():
     browsers = []
 
     #populating all browsers into list
@@ -112,8 +160,10 @@ def reader_profile():
     #returns users list tallied up with the frequency each user id has been found, will show the top 10 most seen users
     return pd.Series(users).value_counts().head(10)
 
+##REQ 5
 
 #search_continent("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0")
 #views_by_browser()
 #reader_profile()
+
 gui()
