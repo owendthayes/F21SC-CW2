6a45d9e2745e78304bdb3f451bc2aeba814b6bdd
jd2030 <jd2030@hw.ac.uk>
2024-12-09 02:30:02 +0000

    yes

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ root = Tk()
 
 ##GUI FOR CHOOSING INPUT FILE
 def gui_load_file():
+    root = Tk()
+    
     root.title("F20SC-CW2 Data Analysis Tracker")
     root.geometry('900x600')
 
@@ -85,10 +87,10 @@ def clicked_load_file(filePath):
         gui_main()
         
 def gui_main():
-    global root
     root = Tk()
-
     ##decide a document to do operations
+
+    states = {"country": False, "continent": False, "views_verbose": False, "views_short": False, "reader": False}
 
     root.title("F20SC-CW2 Data Analysis Tracker")
     root.geometry('900x600')
@@ -105,63 +107,84 @@ def gui_main():
     root.rowconfigure(2, weight = 1)  
 
         
-
-    ##BUTTON FUNCTIONS, NEED CHANGED LATER TO NOT BE HARD CODED
-    def clicked_search_doc_country():
+    #HIDES AND SHOWS BUTTONS ALLOWING FOR GRID TO BE CREATED AVOIDING FORMATTING ISSUES
+    def button_hiding_showing_helper():
         btn_view_document_continent.grid_remove()
         btn_view_document_country.grid_remove()
         btn3.grid_remove()
         btn4.grid_remove()
         btn5.grid_remove()
+        btn_back_to_load.grid_remove()
 
         btn_choose_doc.grid()
         txt_doc.grid()
-        btn_back.grid()
-                   
-    def back_to_load():
-        print("kys")
-        
+        btn_back_to_main.grid()
+    
+    #Helper method to flip the value of the associated key in the states dictionary
+    def toggle_flag(flag):
+        states[flag] = True
+        print(f"Cunt Cunt Cunt: {states['country']}")
+
+    #THESE NEED TESTED, PROBABLY NOT WORKING?
     def back_to_main():
-        btn_back.grid_remove()
+        btn_view_document_continent.grid()
+        btn_view_document_country.grid()
+        btn3.grid()
+        btn4.grid()
+        btn5.grid()
+        btn_back_to_load.grid()
 
-    def clicked_search_doc_continent():
-        create_bar_chart(search_continent("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0"), "140228202800-6ef39a241f35301a9a42cd0ed21e5fb0", "Visitor frequency from each continent for document","Country", "Frequency")
+        btn_choose_doc.grid_remove()
+        txt_doc.grid_remove()
+        btn_back_to_main.grid_remove()
 
-    def clicked_views_by_browser_verbose():
-        create_bar_chart(views_by_browser_verbose(""))
-
-    def clicked_views_by_browser_short():
-        create_bar_chart(views_by_browser_short(""))
+    def back_to_load():
+        root.withdraw()
+        gui_load_file()
 
     #top_frame = Frame(root, height=3, width=4).grid(row=0, column=0, sticky = 'nw', columnspan=5)
     
     ##BUTTONS TO BE USED FOR STUFF
-    btn_view_document_country = Button(root, text = "Search visitors by country", font = ("Arial", 14) ,fg= "red", command=clicked_search_doc_country)
-    btn_view_document_country.grid(row=3, column=0)
+    btn_view_document_country = Button(root, text = "Search visitors by country", font = ("Arial", 14) ,fg= "red", command=lambda: (button_hiding_showing_helper, toggle_flag("country")))
+    btn_view_document_country.grid(row=2, column=0)
     
-    btn_view_document_continent = Button(root, text = "Search document visitors by continent", font = ("Arial", 14) ,fg= "red", command=clicked_search_doc_continent)
-    btn_view_document_continent.grid(row=3,column=1)
+    btn_view_document_continent = Button(root, text = "Search document visitors by continent", font = ("Arial", 14) ,fg= "red", command=lambda: (button_hiding_showing_helper, toggle_flag("continent")))
+    btn_view_document_continent.grid(row=2,column=1)
 
-    btn3 = Button(root, text = "Hit me", font = ("Arial", 14) ,fg= "red", command=clicked_views_by_browser_verbose)
-    btn3.grid(row=3, column=2)
+    btn3 = Button(root, text = "Hit me", font = ("Arial", 14) ,fg= "red", command=lambda: (button_hiding_showing_helper, toggle_flag("continent")))
+    btn3.grid(row=2, column=2)
 
-    btn4 = Button(root, text = "Hit me", font = ("Arial", 14) ,fg= "red", command=clicked_views_by_browser_short)
-    btn4.grid(row=3, column=3)
+    btn4 = Button(root, text = "Hit me", font = ("Arial", 14) ,fg= "red", command=lambda: (button_hiding_showing_helper, toggle_flag("continent")))
+    btn4.grid(row=2, column=3)
 
-    btn5 = Button(root, text = "Hit me", font = ("Arial", 14) ,fg= "red", command=clicked_search_doc_country)
-    btn5.grid(row=3, column=4)   
+    btn5 = Button(root, text = "Hit me", font = ("Arial", 14) ,fg= "red", command=lambda: (button_hiding_showing_helper, toggle_flag("continent")))
+    btn5.grid(row=2, column=4)   
+
+    def the_fixer():
+        if (states.get("country") == True):
+            print("bastard") #i agree
+            freq = search_country(txt_doc.get(1.0, END).strip())
+            title = "Document viewed per country"
+            x = "country"
+            y = "freq"
+            states['country'] = False
+
+        return (freq, txt_doc.get(1.0, END).strip(), title, x, y)
 
     txt_doc = Text(root, font = ("Arial", 14) ,fg= "black", height = 1, width = 20)
     txt_doc.grid(row=0, column=0, sticky = 'nw', columnspan=5)
     txt_doc.grid_remove()
 
-    btn_choose_doc = Button(root, text = "choose document", font = ("Arial", 14) ,fg= "red", command=lambda:create_bar_chart(search_country(txt_doc.get(1.0, END).strip()), txt_doc.get(1.0, END), "Visitor frequency from each country for document","Country", "Frequency"), padx=20)
+    btn_choose_doc = Button(root, text = "choose document", font = ("Arial", 14) ,fg= "red", command=lambda:create_bar_chart(the_fixer), padx=20)
     btn_choose_doc.grid(row=0, column=1, columnspan=5, sticky='n')
     btn_choose_doc.grid_remove()
 
-    btn_back = Button(root, text = "back", font = ("Arial", 14) ,fg= "red", command=lambda:back_to_main, width=10, height=1)
-    btn_back.grid(row=2, column=0, sticky='sw', columnspan=5)
-    btn_back.grid_remove()
+    btn_back_to_main = Button(root, text = "back", font = ("Arial", 14) ,fg= "red", command=lambda:back_to_main, width=10, height=1)
+    btn_back_to_main.grid(row=2, column=0, sticky='sw', columnspan=5)
+    btn_back_to_main.grid_remove()
+
+    btn_back_to_load = Button(root, text = "back to file selection", font = ("Arial", 14) ,fg= "red", command=lambda:back_to_load, width=10, height=1)
+    btn_back_to_load.grid(row=2, column=0, sticky='sw')
 
     root.mainloop()
 
@@ -229,25 +252,44 @@ def views_by_browser_short():
 def reader_profile():
     #creates list of all user ids found in the list allowing for duplicate entries
     users = [user.get("visitor_uuid") for user in data] 
+
     print(pd.Series(users).value_counts().head(10)) #DEBUGGING
     #returns users list tallied up with the frequency each user id has been found, will show the top 10 most seen users
+  
     return pd.Series(users).value_counts().head(10)
 
-##REQ 5
-def users_from_doc(document):
-    ##get list of users that have read the given document
-    ##DONT THINK (DOCUMENT) WILL WORK AS PARAMETER, CHANGE LATER.
-    doc = json.load(document)
-    readers = []
-    for obj in document:    ## this probably will not work, gotta load file
-        if obj.get("vistor_uuid") not in readers:
-            visitorID = obj.get("visitor_uuid")
-            readers.append(visitorID)
-    return readers
 
+##REQ 5
+def users_from_doc(documentID):
+    #get list of users that have read the given document
+    users_viewed = []
+    for obj in data:
+        if obj.get("env_doc_id") is documentID:
+            users_viewed.append(obj.get("visitor_uuid"))
+    return users_viewed
+            
 def docs_from_users(visitorID):
-    ##get list of documents that user has read
-    documents = []
+    #get list of documents that user has read
+    documents_viewed = []
+    for obj in data:
+        if obj.get("visitor_uuid") is visitorID:
+            documents_viewed.append(obj.get("env_doc_id"))
+    return documents_viewed
+
+def also_like(documentID, visitorID, ):
+    #get list of "Liked documents"
+    users = users_from_doc(documentID)
+    
+    all_read_docs = []
+    for user in users:
+        all_read_docs.append(docs_from_users(user))
+
+    
+    
+
+def sorting_function():
+    print("kys")
+
 
 
 #search_continent("140228202800-6ef39a241f35301a9a42cd0ed21e5fb0")
